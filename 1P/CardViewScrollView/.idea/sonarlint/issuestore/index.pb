e3bb3896df6d25f824607e2b5a03df158197d34a
Angel Brambila <malthael.91@gmail.com>
2021-03-02 10:46:59 -0600

    Added CadrView and Scroll View Project

---
+++ 1P/CardViewScrollView/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+s
+Capp/src/main/java/com/logicadifusa/cardviewlayout/MainActivity.java,1\6\16804f4e2af7cf60d1322dcd9bd7dfad48678f47
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ 1P/CardViewScrollView/app/src/main/java/com/logicadifusa/cardviewlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.logicadifusa.cardviewlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
